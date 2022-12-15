80e0c027ec0c8383391875e17cc9917c41df864a
N3question <118955373+N3question@users.noreply.github.com>
2022-12-15 15:30:11 +0000

    [finish]ruby-7章演習

---
+++ length.rb
@@ -0,0 +1,11 @@
+# メゾット・・・複数の処理を１つにまとめて扱いやすくしたもののこと
+# 「length」「reverse」「include?」「methods
+# 文字列型に使用することができる代表的なメソッド
+
+# 前の章で出てきたputsやto_i、to_sなども、メソッド
+
+# START！
+
+puts "WEBCAMP".length
+# 「WEBCAMPの文字列」を「lengthメソッドで文字数を数える」
+# 文字数７
---
+++ reverse.rb
@@ -0,0 +1,5 @@
+# メゾット・・・複数の処理を１つにまとめて扱いやすくしたもののこと
+# reverse・・・文字列が逆から表示
+
+# START!
+puts "WEBCAMP".reverse
